f69f990b39aa4b93326243436f6a0740a7969fd0
Makoto Shinozuka <makoto.shinozuka@rakus-partners.co.jp>
2022-11-11 14:17:25 +0900

    問1

--- src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -8,7 +8,23 @@ class StringUtilsTest {
 
 	@Test
 	void testToSnakeCase1() {
-		String result = 
+		String result = StringUtils.toSnakeCase("aaa");
+		assertEquals("aaa",result,("aaa"));
+	}
+	
+	@Test
+	void testToSnakeCase2() {
+		String result = StringUtils.toSnakeCase("HelloWorld");
+		assertEquals(StringUtils.toSnakeCase("HelloWorld"), result,("Hello_world"));
+	}
+	
+	@Test
+	void testToSnakeCase3() {
+		String result = StringUtils.toSnakeCase("practiceJunit");
+		assertEquals(StringUtils.toSnakeCase("practiceJunit"), result,("practice_junit"));
 	}
 
 }
+
+
+
---
+++ src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
@@ -0,0 +1,16 @@
+package junit.tutorial.ex01.e02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void divide() {
+		System.out.println("divide");
+		int 
+	}
